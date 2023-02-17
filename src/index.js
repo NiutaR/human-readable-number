ad11f13c5c57cf7a40c0d5db398568f5cdb7b5e6
NiutaR <69516148+NiutaR@users.noreply.github.com>
2023-02-17 12:44:31 +0200

    fix: fix the issue for hundrets

--- src/index.js
+++ src/index.js
@@ -1,25 +1,66 @@
 module.exports = function toReadable(number) {
-    let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
-        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
+    let ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
         'seventeen', 'eighteen', 'nineteen'
     ];
+
     let tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
         'ninety'
     ];
-    let numberString = number.toString();
-    if (number === 0) return 'zero'
+    var numberString = number.toString();
+    if (number === 0) return 'zero';
     if (number < 20) {
         return ones[number];
     }
+
     if (numberString.length === 2) {
         return tens[numberString[0]] + ' ' + ones[numberString[1]];
     }
     if (numberString.length === 3) {
-        return ones[numberString[0]] + ' ' + 'hundred' + ' '  + tens[numberString[1]] + ' ' + ones[numberString[2]];
+
+        if (numberString[1] === '0' && numberString[2] === '0') {
+            return ones[Number(numberString[0])] + ' ' + 'hundred';
+        }
+        if (numberString[1] === '1' && numberString[2] === '0') {
+
+            return ones[(numberString[0])] + ' hundred' + ' ' + ones[10];
+        }
+        if (numberString[1] === '1' && numberString[2] === '1') {
+
+            return ones[(numberString[0])] + ' hundred' + ' ' + ones[11];
+        }
+        if (numberString[1] === '1' && numberString[2] === '2') {
+
+            return ones[(numberString[0])] + ' hundred' + ' ' + ones[12];
+        }
+        if (numberString[1] === '1' && numberString[2] === '3') {
+
+            return ones[(numberString[0])] + ' hundred' + ' ' + ones[13];
+        }
+        if (numberString[1] === '1' && numberString[2] === '4') {
+
+            return ones[(numberString[0])] + ' hundred' + ' ' + ones[14];
+        }
+        if (numberString[1] === '1' && numberString[2] === '5') {
+
+            return ones[(numberString[0])] + ' hundred' + ' ' + ones[15];
+        }
+        if (numberString[1] === '1' && numberString[2] === '6') {
+
+            return ones[(numberString[0])] + ' hundred' + ' ' + ones[16];
+        }
+        if (numberString[1] === '1' && numberString[2] === '7') {
+
+            return ones[(numberString[0])] + ' hundred' + ' ' + ones[17];
+        }
+        if (numberString[1] === '1' && numberString[2] === '8') {
+
+            return ones[(numberString[0])] + ' hundred' + ' ' + ones[18];
+        }
+        if (numberString[1] === '1' && numberString[2] === '9') {
+
+            return ones[(numberString[0])] + ' hundred' + ' ' + ones[19];
+        } else {
+            return ones[numberString[0]] + ' hundred' + ' ' + tens[numberString[1]] + ' ' + ones[numberString[2]];
+        }
     }
-    if (numberString.length === 4) {
-        return ones[numberString[0]] + ' ' + 'thousand' + ' ' + ones[numberString[1]] + ' ' + 'hundred' + ' '  + tens[numberString[2]] + ' ' + ones[numberString[3]];
-    }
-    
-    
 }
